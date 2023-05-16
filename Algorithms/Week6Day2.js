1d0f270f08285bad2dbb89d89e894a110ee15298
EdwardFine <edfine03@hotmail.com>
2023-05-16 08:39:52 -0700

    var>let

--- Algorithms/Week6Day2.js
+++ Algorithms/Week6Day2.js
@@ -32,11 +32,11 @@ const expected4 = [42, 0, 6];
  */
 function interleaveArrays(arr1, arr2) {
     if(arr1.length > arr2.length){
-        var longest = arr1.length;
+        let longest = arr1.length;
     }else{
-        var longest = arr2.length
+        let longest = arr2.length
     }
-    var interleave = []
+    let interleave = []
     for(var i = 0; i < longest; i++){
         if(i < arr1.length){
             interleave.push(arr1[i]);
